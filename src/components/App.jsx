79cc9af4c96ad2a05636c58ed9e788002945638e
Yana Vozniuk <yana.vozniuk@ukr.net>
2023-02-18 20:36:24 +0200

    hw
    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,14 @@ import Filter from './Filter';
 import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || []
+  );
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = task => {
     const searchSameName = contacts.find(contact => contact.name === task.name);
@@ -45,10 +46,6 @@ const App = () => {
       prevContacts.filter(({ id }) => id !== contactId)
     );
   };
-
-  useEffect(() => {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-  }, [contacts]);
 
   return (
     <div>
